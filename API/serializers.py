f5e9a1d7cf0f797565ad88e5729679c57f8542cc
tamil08mctv <tamil08mctv@gmail.com>
2024-07-17 18:16:20 +0530

    API configured

--- API/serializers.py
+++ API/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Machines
 
 class MachinesSerializer(serializers.ModelSerializer):
-    class Meta          :
+    class Meta:
         model= Machines
         fields= ['id','name','type','initiated_date']
 
