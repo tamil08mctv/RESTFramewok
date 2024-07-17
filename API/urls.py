b2a61e199cb5e093b97ba09caef9852d64882051
tamil08mctv <tamil08mctv@gmail.com>
2024-07-17 18:13:56 +0530

    modified

--- API/urls.py
+++ API/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('Machines/',views.MachinesListCreate.as_view(),name="Machines-view-create"),
     path('Machines/<int:id>/',views.MachinesRetrieveUpdateDestroy.as_view(),name="update ")
-
 ]
